f1a78b8da766852afc89e8a5aa448e8eb03b3f8b
nboaram <nboaram1@hotmail.co.uk>
2019-07-12 14:32:25 +0100

    angular build

---
+++ angularInstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "building angular project"
+ng build
+project=$(ls dist)
+echo "makin sure nginx is installed"
+sudo apt install -y nginx
+echo "configuring nginx"
+sed "s|{{WEB_ROOT}}|$(pwd)/dist/${project{|g" nginx.conf | sudo tee /etc/nginx/nginx.conf
+sudo nginx -s relo
