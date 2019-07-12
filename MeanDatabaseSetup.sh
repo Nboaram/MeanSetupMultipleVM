10e9c93ab5fd6907ae7d463be4e32191a7eab445
nboaram <nboaram1@hotmail.co.uk>
2019-07-12 13:34:06 +0100

    Adding Database Config File

--- MeanDatabaseSetup.sh
+++ MeanDatabaseSetup.sh
@@ -5,4 +5,5 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD7
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+sudo cp mongod.conf /etc/mongod.conf
 sudo service mongod start
